37b9c35840ad81167283a7a9c8eeb71a1c8c42c8
timotheekim10 <timotheekim10@gmail.com>
2024-07-11 14:19:38 +0900

    숫자를 정렬하자

---
+++ 01900/SWEA_1966.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int T;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            System.out.print("#" + t + " ");
+            for (int x : arr) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
